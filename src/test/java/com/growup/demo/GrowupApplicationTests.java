ff62cf1a70ab605469cc377dae5c4fd609c4bbb4
kanghana1 <qazplm12093@naver.com>
2025-02-12 18:52:20 +0900

    init setting

---
+++ src/test/java/com/growup/demo/GrowupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
